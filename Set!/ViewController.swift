09e3b4a332b0e4ba357b177e995490afe01e6fa6
Mark Johnson <myjohns2@ncsu.edu>
2018-06-28 16:26:44 +0800

    Initial Commit

---
+++ Set!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Set!
+//
+//  Created by Mark Johnson on 6/28/18.
+//  Copyright © 2018 Mark Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
